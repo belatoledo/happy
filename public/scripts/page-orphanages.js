90acdc1f36b2d8403b6f5c92e3a23696cea25bf5
Izabela Toledo <it.amancio@gmail.com>
2020-10-17 01:50:23 -0300

    integracao da funcao addmaker com o fakedata na page orphanages

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -7,21 +7,40 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 //create icon
 
 const icon = L.icon ({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [40, 50],
     iconAnchor: [12, 68],
     popupAnchor: [155, 2]
 })
 
-//create popup overlay
+//add a function
+function addMarker({id, name, lat, lng}) {
 
+//create popup overlay
 const popup = L.popup ({
     closeButton: false,
     className: 'map-popup',
     minWidth: 200, 
     minHeight: 200
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+}).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`)
+
 
 //create and add marker
-L.marker([-19.9214549,-43.9616234], {icon}).addTo(map)
+L.marker([lat,lng], {icon}).addTo(map)
     .bindPopup(popup);
+
+}
+
+//executando a function
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+        
+    }
+    
+    addMarker(orphanage)
+})
